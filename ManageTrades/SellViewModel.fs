e936de8b7cb835d41111a34035c4514f459dd5ca
Bizmonger <scott.nimrod@bizmonger.net>
2017-01-31 09:15:59 -0500

    Set enable/disable state on Confirm button based on valid input

--- ManageTrades/SellViewModel.fs
+++ ManageTrades/SellViewModel.fs
@@ -16,22 +16,34 @@ type SellViewModel(info:SharesInfo) as this =
     let broker =     getBroker() :> IBroker
 
     let mutable sellQty = ""
+    let mutable canSell = false
 
-    let confirm =
-        DelegateCommand( (fun _ -> 
-
-            if   Int32.Parse this.SellQty > 0 && Int32.Parse this.SellQty <= this.Shares
-            then dispatcher.ConfirmSell { AccountId = accountId
-                                          Symbol    = this.Symbol
-                                          Quantity  = Int32.Parse this.SellQty } ) ,
-                          fun _ -> true ) :> ICommand
+    let confirm = DelegateCommand( (fun _ -> this.ConfirmSell()) ,
+                                    fun _ -> true ) :> ICommand
 
     member this.Symbol     with get() = info.Shares.Symbol
     member this.Shares     with get() = info.Shares.Qty
     member this.StockPrice with get() = info.PricePerShare
     member this.Total      with get() = ((decimal)info.Shares.Qty * info.PricePerShare)
 
-    member this.SellQty    with get() =      sellQty
-                           and  set(value) = sellQty <- value
+    member this.SellQty    
+        with get() =      sellQty
+        and  set(value) = sellQty <- value
 
+                          let success , validQty = Int32.TryParse sellQty
+                          if  success then this.CanSell <- validQty >  0 && 
+                                                           validQty <= this.Shares
+                          else this.CanSell <- false
+                          base.NotifyPropertyChanged(<@ this.SellQty @>)
+        
+    member this.CanSell   with get() = canSell
+                          and  set(value) = canSell <- value
+                                            base.NotifyPropertyChanged(<@ this.CanSell @>)
     member this.Confirm = confirm
+
+    member private this.ConfirmSell() =
+        
+        if this.CanSell then
+            dispatcher.ConfirmSell { AccountId = accountId
+                                     Symbol    = this.Symbol
+                                     Quantity  = Int32.Parse this.SellQty }
