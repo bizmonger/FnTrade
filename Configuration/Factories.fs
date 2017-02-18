a66fbb6e5516ef826693c8dcb132cbbcffa41152
Bizmonger <scott.nimrod@bizmonger.net>
2017-02-18 13:10:13 -0500

    Removed dead code

--- Configuration/Factories.fs
+++ Configuration/Factories.fs
@@ -3,11 +3,9 @@
 open Services
 open TestAPIImpl
 
-//let broker =     MockBroker()
 let dispatcher = Dispatcher()
 let accountId =  "Bizmonger"
 
 (*Functions*)
 let getDispatcher() = dispatcher
-//let getBroker() =     broker
 let getAccountId() =  accountId
--- ManageTrades/RequestBuyConfirmedViewModel.fs
+++ ManageTrades/RequestBuyConfirmedViewModel.fs
@@ -5,6 +5,9 @@ open Core.Entities
 open Services
 open Integration.Factories
 open TestAPIImpl
+open System.Net.Http
+open System
+open System.Net.Http.Headers
 
 type RequestBuyConfirmedViewModel(request:RequestInfo) =
 
@@ -18,6 +21,16 @@ type RequestBuyConfirmedViewModel(request:RequestInfo) =
                          and  set(value) = total <- value
                                            base.NotifyPropertyChanged(<@ this.Total @>)
     member this.Load() =
+//        let client = new HttpClient()
+//        client.BaseAddress <- Uri("http://localhost:48213/api/transaction");
+//        client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue("application/json"));
+//
+//        let response = client.GetAsync("api/yourcustomobjects").Result;
+//        if response.IsSuccessStatusCode
+//        then let result = response.Content.ReadAsStringAsync().RunSynchronously
+//             ()
+//        else failwith "Something bad happened"
+
         getInfo request.Symbol |> function
         | Some info -> this.Total <- info.Price * (decimal) request.Quantity
         | None      -> failwith   <| sprintf "Failed to retrieve stock information for %s" this.Symbol
